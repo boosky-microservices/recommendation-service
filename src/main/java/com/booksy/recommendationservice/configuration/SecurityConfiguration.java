8a3e0d715a11ea7c3eea80d1af2e3fba45cf72fb
YassineCherifi <mohyassine.cherifi@gmail.com>
2020-11-17 19:56:24 +0100

    check jwt token

---
+++ src/main/java/com/booksy/recommendationservice/configuration/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.booksy.recommendationservice.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    String issuerUri;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .anyRequest().permitAll()
+
+                )
+
+                .oauth2ResourceServer(oauth2ResourceServer ->
+                        oauth2ResourceServer
+                                .jwt(jwt ->
+                                        jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))
+                                )
+                );
+    }
+}
--- src/main/java/com/booksy/recommendationservice/controllers/RecombeeController.java
+++ src/main/java/com/booksy/recommendationservice/controllers/RecombeeController.java
@@ -1,13 +1,14 @@
 package com.booksy.recommendationservice.controllers;
 
 
-import com.booksy.recommendationservice.models.Book;
 import com.booksy.recommendationservice.models.RecommendedBook;
 import com.booksy.recommendationservice.services.RecombeeService;
 import com.recombee.api_client.exceptions.ApiException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,12 +20,12 @@ public class RecombeeController {
 
     private final RecombeeService recombeeService;
 
-
-
     @GetMapping("/books")
-    public ResponseEntity<List<RecommendedBook>> getRecommendedBooks(@RequestParam String bookId, @RequestParam String userId, @RequestParam int count) throws ApiException {
-        return new ResponseEntity<>(recombeeService.getRecommendedBooksFromBook(bookId,userId,count), HttpStatus.OK);
+    public ResponseEntity<List<RecommendedBook>> getRecommendedBooks(@AuthenticationPrincipal Jwt jwt,
+                                                                     @RequestParam String bookId,
+                                                                     @RequestParam int count
+    ) throws ApiException {
+        String userId = jwt != null && jwt.getClaim("sub") != null ? jwt.getClaim("sub").toString().split("\\|")[1] : "noid";
+        return new ResponseEntity<>(recombeeService.getRecommendedBooksFromBook(bookId, userId, count), HttpStatus.OK);
     }
-
-
 }
