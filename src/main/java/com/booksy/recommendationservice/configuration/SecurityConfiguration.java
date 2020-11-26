601b0b6ae7b800ab57642542c8d4c9f78882a17b
YassineCherifi <mohyassine.cherifi@gmail.com>
2020-11-26 18:58:05 +0100

    change cors support way

--- src/main/java/com/booksy/recommendationservice/configuration/SecurityConfiguration.java
+++ src/main/java/com/booksy/recommendationservice/configuration/SecurityConfiguration.java
@@ -24,8 +24,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .and()
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
                                 .anyRequest().permitAll()
