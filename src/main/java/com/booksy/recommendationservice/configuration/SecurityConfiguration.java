f7cc729c5fc739d170eec94d9a0c312b78709507
YassineCherifi <mohyassine.cherifi@gmail.com>
2020-11-26 17:13:12 +0100

    change cors support way

--- src/main/java/com/booksy/recommendationservice/configuration/SecurityConfiguration.java
+++ src/main/java/com/booksy/recommendationservice/configuration/SecurityConfiguration.java
@@ -1,11 +1,18 @@
 package com.booksy.recommendationservice.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @EnableWebSecurity
 @Configuration
@@ -31,5 +38,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                                         jwt.decoder(JwtDecoders.fromIssuerLocation(issuerUri))
                                 )
                 );
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Collections.singletonList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
